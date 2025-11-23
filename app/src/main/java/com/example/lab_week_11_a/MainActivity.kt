a374723a8c880c0bdbe7c7c1b2bb76a08c646e85
raphaelvittorio <raphaelvittoriomulyo@gmail.com>
2025-11-23 22:18:55 +0700

    Commit No. 1: Building App with SharedPreferences and DataStore

--- app/src/main/java/com/example/lab_week_11_a/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_11_a/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.lab_week_11_a
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Menggunakan SettingsApplication dan SettingsStore
+        val preferenceWrapper = (application as SettingsApplication).settingsStore
+
+        val preferenceViewModel = ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return SettingsViewModel(preferenceWrapper) as T
+            }
+        })[SettingsViewModel::class.java]
+
+        preferenceViewModel.textLiveData.observe(this) {
+            findViewById<TextView>(R.id.activity_main_text_view).text = it
+        }
+
+        // Listener tombol sama seperti sebelumnya, menggunakan saveText dari ViewModel
+        findViewById<Button>(R.id.activity_main_button).setOnClickListener {
+            preferenceViewModel.saveText(
+                findViewById<EditText>(R.id.activity_main_edit_text).text.toString()
+            )
         }
     }
 }
